5e20d675815356f9ec6a3d218ef6e88264ace214
Parveen Yadav <parveeenrao7727@gmail.com>
2025-02-17 11:48:47 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name = 'machine_learning project',
+    version = '0.0.1',
+    author = 'Parveen',
+    author_email = 'parveeenrao7727@gmail.com',
+    packages = find_packages(),
+    install_requires = []   
+)
